6b1ba6810b9e59afa4be661090033a9f946b4431
Sam Hung <samhung0411@outlook.com>
2021-08-27 12:49:03 +1000

    my first test

---
+++ lib/http_test.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+void pressedButton() async {
+  final url = Uri.parse("http://localhost:3000/");
+  final response = await http.get(url);
+  print(response.body);
+}
